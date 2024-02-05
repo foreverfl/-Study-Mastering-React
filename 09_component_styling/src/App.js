6834b304d4f7556495e3a4511f8f6151995a0d6c
foreverfl <forever_fl@naver.com>
2024-02-06 06:11:47 +0900

    Is completeing to the chapter 10

--- 09_component_styling/src/App.js
+++ 09_component_styling/src/App.js
@@ -1,11 +1,16 @@
-import React, { Component } from 'react';
-import SassComponent from './SassComponent';
+import React, { Component } from "react";
+import SassComponent from "./SassComponent";
+import CSSModule from "./CSSModule";
 
 class App extends Component {
   render() {
     return (
       <div>
+        <h1>SASS</h1>
         <SassComponent />
+        <hr></hr>
+        <h1>CSS Module</h1>
+        <CSSModule />
       </div>
     );
   }
